7ba5d4436c64ae577b0b6b2e469614574ab21581
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-03-03 13:32:11 +0100

    add favorit.js

---
+++ favorit.js
@@ -0,0 +1,14 @@
+document.querySelector(".destinationlist").addEventListener("click", (e) => {
+    if (e.target.classList.contains("fa-heart")) {
+    let destinationsId = e.target.closest("figure").attributes.id.value;
+      if (!localStorage.getItem(destinationsId, destinationsId)) {
+        localStorage.setItem(destinationsId, destinationsId);
+        e.target.classList.add("fa-solid");
+      } else {
+        localStorage.removeItem(destinationsId);
+        e.target.classList.remove("fa-solid");
+        e.target.classList.add("fa-regular");
+      }
+    }
+  })
+  

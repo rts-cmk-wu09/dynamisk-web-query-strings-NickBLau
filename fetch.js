47d9ed3ae79f57927fb0cf1f5d740cb79aafbfb5
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-03-03 12:02:22 +0100

    add script to fetch.js

---
+++ fetch.js
@@ -0,0 +1,18 @@
+const destinationlist = document.querySelector('.destinationlist')
+
+fetch('./data/destinations.json')
+    .then((response) => response.json())
+    .then(data => {
+        // console.log(data.destinations[0]);
+        data.destinations.forEach(destination => {
+            destinationlist.innerHTML += `
+            <figure id="${destination.id}">
+            <img src="./img/${destination.image}" alt="">
+            <div>
+            <span><i class="fa-regular fa-heart"></i></span>
+            <a href="/destination.html?id=${destination.id}">More</a>
+            </div>
+            </figure>
+            `;
+        });
+    });
